823cb0f44e2e4697b4df26970e688f16f1a8bf70
rholbrook <rholbrook@microfocus.com>
2020-03-11 14:59:44 -0600

    Updated the yaml/controller for the grocerylist portion of our API

--- mealSocial-dev/controllers/groceryList-controller.js
+++ mealSocial-dev/controllers/groceryList-controller.js
@@ -1,19 +1,86 @@
+const data = require('../data/databaseConnection');
+
 exports.getGroceryLists = function(req, res, next) {
-    res.send("Get GroceryLists!")
+    let db = data.openDataConnection();
+    let sql = 'SELECT * FROM GroceryList';
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        res.send(rows);
+    });
+
+    data.closeDataConnection(db);
 }
 
 exports.createGroceryList = function(req, res, next) {
-    res.send("Create new GroceryList!")
+    let db = data.openDataConnection();
+    let params = [req.body.ingredients, req.body.notes];
+    let sql = 'INSERT INTO GroceryList(ingredients, notes) VALUES (?, ?)';
+
+    db.run(sql, params, (err) => {
+        if (err) {
+            console.log(err.message);
+            res.sendStatus(400);
+        }
+        else {
+            res.sendStatus(201);
+        }
+      });
+
+    data.closeDataConnection(db);
 }
 
 exports.getGroceryListById = function(req, res, next) {
-    res.send("Get GroceryList by Id!")
+    let db = data.openDataConnection();
+    let sql = 'SELECT * FROM GroceryList WHERE id=?';
+
+    db.get(sql, req.params.id, (err, row) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        res.send(row);
+    });
+
+    data.closeDataConnection(db);
 }
 
 exports.editGroceryList = function(req, res, next) {
-    res.send("Edit GroceryList by Id!")
+    let db = data.openDataConnection();
+    let params = [req.body.ingredients, req.body.notes, req.params.id];
+    let sql = 'UPDATE GroceryList SET ingredients=?, notes=? WHERE id=?';
+    
+    console.log(sql);
+
+    db.run(sql, params, (err) => {
+        if (err) {
+            console.log(err.message);
+            res.sendStatus(400);
+        }
+        else {
+            res.sendStatus(200);
+        }
+    });
+
+    data.closeDataConnection(db);
 }
 
 exports.deleteGroceryList = function(req, res, next) {
-    res.send("Delete GroceryList By Id!")
+    let db = data.openDataConnection();
+    let sql = 'DELETE FROM GroceryList WHERE id=?';
+
+    db.run(sql, req.params.id, (err) => {
+        if(err) {
+            console.log(err.message);
+            res.sendStatus(400);
+        }
+        else {
+            res.sendStatus(200);
+        }
+    });
+
+    data.closeDataConnection(db);
 }
