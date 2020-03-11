f069c3968fb2118e289f118ad48df5e90621b6a4
rholbrook <rholbrook@microfocus.com>
2020-03-11 16:00:13 -0600

    Fixed the yaml and added the ingredient portion of our api

--- mealSocial-dev/controllers/ingredients-controller.js
+++ mealSocial-dev/controllers/ingredients-controller.js
@@ -1,23 +1,86 @@
+const data = require('../data/databaseConnection');
+
 exports.getIngredients = function(req, res, next) {
-    res.send("Get Ingredients !")
+    let db = data.openDataConnection();
+    let sql = 'SELECT * FROM Ingredient';
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        res.send(rows);
+    });
+
+    data.closeDataConnection(db);
 }
 
 exports.createIngredient = function(req, res, next) {
-    res.send("Create new Ingredient!")
-}
+    let db = data.openDataConnection();
+    let params = [req.body.ingredientList];
+    let sql = 'INSERT INTO Ingredient(ingredientList) VALUES (?)';
 
-exports.getIngredientList = function(req, res, next) {
-    res.send("Get All Ingredients in a Recipe!")
+    db.run(sql, params, (err) => {
+        if (err) {
+            console.log(err.message);
+            res.sendStatus(400);
+        }
+        else {
+            res.sendStatus(201);
+        }
+      });
+
+    data.closeDataConnection(db);
 }
 
 exports.getIngredientById = function(req, res, next) {
-    res.send("Get Ingredient by Id!")
+    let db = data.openDataConnection();
+    let sql = 'SELECT * FROM Ingredient WHERE id=?';
+
+    db.get(sql, req.params.id, (err, row) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        res.send(row);
+    });
+
+    data.closeDataConnection(db);
 }
 
 exports.editIngredient = function(req, res, next) {
-    res.send("Edit Ingredient by Id!")
+    let db = data.openDataConnection();
+    let params = [req.body.ingredientList, req.params.id];
+    let sql = 'UPDATE Ingredient SET ingredientList=?  WHERE id=?';
+    
+    console.log(sql);
+
+    db.run(sql, params, (err) => {
+        if (err) {
+            console.log(err.message);
+            res.sendStatus(400);
+        }
+        else {
+            res.sendStatus(200);
+        }
+    });
+
+    data.closeDataConnection(db);
 }
 
 exports.deleteIngredient = function(req, res, next) {
-    res.send("Delete Ingredient By Id!")
+    let db = data.openDataConnection();
+    let sql = 'DELETE FROM Ingredient WHERE id=?';
+
+    db.run(sql, req.params.id, (err) => {
+        if(err) {
+            console.log(err.message);
+            res.sendStatus(400);
+        }
+        else {
+            res.sendStatus(200);
+        }
+    });
+
+    data.closeDataConnection(db);
 }
