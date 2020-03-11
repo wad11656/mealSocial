a9acca3c4689bd67c4be692dddc7c4d56eafe340
rholbrook <rholbrook@microfocus.com>
2020-03-11 14:42:32 -0600

    Fixed yaml and made controller functional for User portion of the api

--- mealSocial-dev/controllers/user-controller.js
+++ mealSocial-dev/controllers/user-controller.js
@@ -1,19 +1,86 @@
+const data = require('../data/databaseConnection');
+
 exports.getUsers = function(req, res, next) {
-    res.send("Get Users!")
+    let db = data.openDataConnection();
+    let sql = 'SELECT * FROM User';
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        res.send(rows);
+    });
+
+    data.closeDataConnection(db);
 }
 
 exports.createUser = function(req, res, next) {
-    res.send("Create new User!")
+    let db = data.openDataConnection();
+    let params = [req.body.email, req.body.username, req.body.avatarURL, req.body.mealplans, req.body.recipes];
+    let sql = 'INSERT INTO User(email, username, avatarURL, mealplans, recipes) VALUES (?, ?, ?, ?, ?)';
+
+    db.run(sql, params, (err) => {
+        if (err) {
+            console.log(err.message);
+            res.sendStatus(400);
+        }
+        else {
+            res.sendStatus(201);
+        }
+      });
+
+    data.closeDataConnection(db);
 }
 
 exports.getUserById = function(req, res, next) {
-    res.send("Get User by Id!")
+    let db = data.openDataConnection();
+    let sql = 'SELECT * FROM User WHERE id=?';
+
+    db.get(sql, req.params.id, (err, row) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        res.send(row);
+    });
+
+    data.closeDataConnection(db);
 }
 
 exports.editUser = function(req, res, next) {
-    res.send("Edit User by Id!")
+    let db = data.openDataConnection();
+    let params = [req.body.email, req.body.username, req.body.avatarURL, req.body.mealplans, req.body.recipes, req.params.id];
+    let sql = 'UPDATE User SET email=?, username=?, avatarURL=?, mealplans=?, recipes=?  WHERE id=?';
+    
+    console.log(sql);
+
+    db.run(sql, params, (err) => {
+        if (err) {
+            console.log(err.message);
+            res.sendStatus(400);
+        }
+        else {
+            res.sendStatus(200);
+        }
+    });
+
+    data.closeDataConnection(db);
 }
 
 exports.deleteUser = function(req, res, next) {
-    res.send("Delete User By Id!")
+    let db = data.openDataConnection();
+    let sql = 'DELETE FROM User WHERE id=?';
+
+    db.run(sql, req.params.id, (err) => {
+        if(err) {
+            console.log(err.message);
+            res.sendStatus(400);
+        }
+        else {
+            res.sendStatus(200);
+        }
+    });
+
+    data.closeDataConnection(db);
 }
