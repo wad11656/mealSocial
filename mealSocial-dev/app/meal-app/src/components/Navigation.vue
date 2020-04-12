5edcbb55a06854d83ad043b5a04a277156ae92a5
rholbrook <rholbrook@microfocus.com>
2020-04-12 17:49:51 -0600

    Added Authentication

---
+++ mealSocial-dev/app/meal-app/src/components/Navigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="bar">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/login" v-if="!loggedIn">Login</router-link>
+      <span @click="handleLogout" v-else>Logout</span>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+#bar {
+  background-color: yellow;
+}
+</style>
+<script>
+export default {
+  props: ["loggedIn"],
+  data() {
+    return {};
+  },
+  methods: {
+    handleLogout(e) {
+      localStorage.removeItem("jwt");
+      localStorage.removeItem("user");
+      this.$emit("loggedout");
+      if (this.$router.currentRoute.path != "/") {
+        this.$router.push("/");
+      }
+    }
+  }
+};
+</script>
--- mealSocial-dev/app/meal-app/src/router/index.js
+++ mealSocial-dev/app/meal-app/src/router/index.js
@@ -1,39 +1,104 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from "vue-router"
 import Recipe from '../views/Recipe.vue'
 import MealPlans from '../views/MealPlan.vue'
 import GroceryList from '../views/GroceryList.vue'
+import HelloWorld from "@/components/HelloWorld";
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import UserBoard from "@/components/UserBoard";
+import Admin from "@/components/Admin";
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/recipes',
-    name: 'Recipes',
-    component: Recipe
-  }, 
-  {
-    path: '/mealplans',
-    name: 'mealplans',
-    component: MealPlans
-  },
-  {
-    path: '/grocerylist',
-    name: 'grocerylist',
-    component: GroceryList
+  let router = new Router ({
+    mode: "history",
+    routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/recipes',
+      name: 'Recipes',
+      component: Recipe
+    }, 
+    {
+      path: '/mealplans',
+      name: 'mealplans',
+      component: MealPlans
+    },
+    {
+      path: '/grocerylist',
+      name: 'grocerylist',
+      component: GroceryList
+    },
+    {
+      path: "/userboard",
+      name: "userboard",
+      component: UserBoard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: Admin,
+      meta: {
+        requiresAuth: true,
+        is_admin: true
+      }
+    }
+]});
+
+
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem("jwt") == null) {
+      next({
+        path: "/login",
+        params: { nextUrl: to.fullPath }
+      });
+    } else {
+      let user = JSON.parse(localStorage.getItem("user"));
+      if (to.matched.some(record => record.meta.is_admin)) {
+        if (user.is_admin == 1) {
+          next();
+        } else {
+          next({ name: "userboard" });
+        }
+      } else {
+        next();
+      }
+    }
+  } else if (to.matched.some(record => record.meta.guest)) {
+    if (localStorage.getItem("jwt") == null) {
+      next();
+    } else {
+      next({ name: "userboard" });
+    }
+  } else {
+    next();
   }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+});
 
 export default router
